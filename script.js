5e9f9a4e626bc01f51ae42c2ba1f7ce5b2f2fc4d
ishita18varshney <ishita18varshney@gmail.com>
2026-09-21 03:09:24 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// Welcome message
+console.log("Welcome to Ishita Varshney's Portfolio!");
+
+// Display current year automatically in the footer
+const year = new Date().getFullYear();
+
+console.log("Portfolio Year: " + year);
